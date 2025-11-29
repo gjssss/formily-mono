b0e1f3231e34133506f65d80998bbf75c1d4fc88
gjssss <gaojinsong2010@gmail.com>
2025-11-29 23:57:26 +0800

    chore: add Render component to the designer module

--- packages/designer/src/components/index.ts
+++ packages/designer/src/components/index.ts
@@ -2,11 +2,13 @@ import CanvasComponent from './canvas.vue'
 import ConfigPanelComponent from './configPanel/index.vue'
 import DesignerComponent from './designer.vue'
 import MaterialPanelComponent from './materialPanel.vue'
+import RenderComponent from './render/index.vue'
 
 export const Canvas = CanvasComponent
 export const ConfigPanel = ConfigPanelComponent
 export const MaterialPanel = MaterialPanelComponent
 export const Designer = DesignerComponent
+export const Render = RenderComponent
 
 // 导出 core
 export * from '../core'
